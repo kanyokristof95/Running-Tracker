9d0f164e59006b8b9b7a85116b239bc46dde81a6
antalist <antal.istvan95@gmail.com>
2018-03-02 20:35:18 +0100

    Kódfájlok létrehozása

---
+++ Source/Running-Tracker/Running-Tracker/OldRunningActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Running_Tracker
+{
+    [Activity(Label = "OldRunningActivity")]
+    public class OldRunningActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
