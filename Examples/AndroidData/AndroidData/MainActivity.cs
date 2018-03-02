da173288059f9c357bf9753c80e7cacc3579403f
Kristóf Kanyó <kanyokristof95@inf.elte.hu>
2018-03-02 15:33:35 +0100

    AndroidData
    A program bemutatja, hogy hogyan lehet adatokat menteni, illetve betölteni (nem csak egyszerű stringeket, hanem tetszőleges típusú objektumot)

---
+++ Examples/AndroidData/AndroidData/MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AndroidData
+{
+    [Activity(Label = "AndroidData", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button submitButton = FindViewById<Button>(Resource.Id.SubmitButton);
+            submitButton.Click += SubmitButton_Click;
+
+            Button viewContactButton = FindViewById<Button>(Resource.Id.viewContactButton);
+            viewContactButton.Click += ViewContactButton_Click;
+        }
+        
+        private void SubmitButton_Click(object sender, System.EventArgs e)
+        {
+            // Get data
+            EditText nameBox = FindViewById<EditText>(Resource.Id.nameBox);
+            string name = nameBox.Text;
+            EditText phoneBox = FindViewById<EditText>(Resource.Id.phoneBox);
+            string phone = phoneBox.Text;
+
+            Contact contact = new Contact(name, phone);
+
+            // Add the new contact to the share preferences
+            var localContacts = Application.Context.GetSharedPreferences("MyContacts", FileCreationMode.Private);
+            var contactEdit = localContacts.Edit();
+
+            List<string> list = new List<string>();
+            ICollection<string> collection = localContacts.GetStringSet("ContactList", null);
+
+            if (collection != null) {
+                list = collection.ToList();
+            }
+            
+            list.Add(JsonConvert.SerializeObject(contact));
+            
+            contactEdit.PutStringSet("ContactList", list);
+
+            contactEdit.Commit();
+
+            // Create a toast notification
+            Android.Widget.Toast.MakeText(this, "Item Added", ToastLength.Short).Show();
+
+            // Cleat boxes
+            nameBox.Text = "";
+            phoneBox.Text = "";
+
+        }
+        
+        private void ViewContactButton_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(ViewContactsActivity));
+            StartActivity(intent);
+        }
+    }
+}
+
---
+++ Examples/AndroidData/AndroidData/ViewContactsActivity.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace AndroidData
+{
+    [Activity(Label = "ViewContactsActivity")]
+    public class ViewContactsActivity : Activity
+    {
+        ListView listView;
+        ArrayAdapter<Contact> ListAdapter;
+
+        List<Contact> contactList;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ViewContacts);
+
+            listView = FindViewById<ListView>(Resource.Id.contentListView);
+
+            contactList = new List<Contact>();
+            var localContacts = Application.Context.GetSharedPreferences("MyContacts", FileCreationMode.Private);
+
+            ICollection<string> collection = localContacts.GetStringSet("ContactList", null);
+            if (collection != null)
+            {
+                List<string> contactStringList = collection.ToList();
+
+                foreach (string contactString in contactStringList)
+                {
+                    Contact contact = JsonConvert.DeserializeObject<Contact>(contactString);
+                    contactList.Add(contact);
+                }
+            }
+
+            // Add the list to the list adapter
+            contactList.Sort((a, b) => string.Compare(a.Name, b.Name));
+            ListAdapter = new ArrayAdapter<Contact>(this, Android.Resource.Layout.SimpleExpandableListItem1, contactList);
+
+            listView.Adapter = ListAdapter;
+            listView.ItemClick += ListView_ItemClick;
+            listView.ItemLongClick += ListView_ItemLongClick;
+        }
+        
+        private void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.SetTitle("TODO");
+            alertDialog.SetMessage("TODO");
+            alertDialog.Show();
+        }
+
+        private void ListView_ItemLongClick(object sender, AdapterView.ItemLongClickEventArgs e)
+        {
+            var item = contactList[e.Position];
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.SetTitle("Contact");
+            alertDialog.SetMessage("Name: " + item.Name + "\nPhone number: " + item.PhoneNumber);
+            alertDialog.SetNegativeButton("Delete", delegate
+            {
+                contactList.Remove(item);
+                ListAdapter = new ArrayAdapter<Contact>(this, Android.Resource.Layout.SimpleExpandableListItem1, contactList);
+
+
+                var localContacts = Application.Context.GetSharedPreferences("MyContacts", FileCreationMode.Private);
+                var contactEdit = localContacts.Edit();
+
+                List<string> list = new List<string>();
+
+                foreach (Contact contact in contactList)
+                {
+                    list.Add(JsonConvert.SerializeObject(contact));
+                }
+
+                contactEdit.PutStringSet("ContactList", list);
+
+                contactEdit.Commit();
+            });
+
+            alertDialog.SetPositiveButton("OK", delegate
+            {
+                // SKIP
+            });
+            alertDialog.Show();
+        }
+    }
+}
