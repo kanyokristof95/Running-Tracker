6ea03f63f8057ca3304c3526b6258dde698f9f7d
Kristóf Kanyó <kanyokristof95@inf.elte.hu>
2018-04-17 13:13:24 +0200

    UnitTest alapja
    Új projekt hozzáadása, amely tartalmazza az Initialize függvényt, és 3 példa tesztet. A value változó is a példa miatt van benne.

---
+++ Source/Running-Tracker/UnitTest/UnitTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Running_Tracker.Model;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        #region Field
+
+        private RunningTrackerModel model;
+
+        private int value; // Test
+
+        #endregion
+
+
+        #region Initialize
+
+        /// <summary>
+        /// Initialize the model
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            model = new RunningTrackerModel(null);
+
+            value = 0; // Test
+        }
+
+        #endregion
+
+
+        #region  Tests
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            value++; // 1
+            Assert.AreEqual(value, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            value++; // 1
+            Assert.AreEqual(value, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.Fail();
+        }
+
+        #endregion
+    }
+}
